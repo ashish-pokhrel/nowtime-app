06b04ebc3c7887c09e14bfc032777c8e9a3e260b
Ashish Pokhrel <ashishpokherl45@gmail.com>
2025-01-17 00:17:58 -0600

    Constants

--- src/constant/constants.tsx
+++ src/constant/constants.tsx
@@ -1,5 +1,5 @@
 export const EXPIRE_MINUTES = 1;
-export const API_URL = "https://localhost:7288/api";
+export const API_URL = "https://grouphoppers.azurewebsites.net/api";
 export const IP_LOCATION_URL = "https://ipinfo.io/json?token=a3ca5fd133d863";
 
 export const accessTokenLocalStorage = "accessToken";
@@ -9,5 +9,5 @@ export const tokenExpiresInLocalStorage = "tokenExpiresIn";
 export const userLocationLocalStorage = "userLocation";
 export const displayLocationLocalStorage = " displayLocation"
 
-export const SignalR_URL = "https://localhost:7288/chatHub";
+export const SignalR_URL = "https://grouphoppers.azurewebsites.net/chatHub";
 export const GOOGLE_CLIENT_ID = "48728014273-1aad0j7h1ep080sb8hr6bdtt497crb6i.apps.googleusercontent.com";
