e18de5401979f5768e2d795da664b958997f8468
Ashish Pokhrel <ashishpokherl45@gmail.com>
2025-01-19 14:17:56 -0600

    Fix environment

--- src/constant/constants.tsx
+++ src/constant/constants.tsx
@@ -1,5 +1,4 @@
 export const EXPIRE_MINUTES = 1;
-export const API_URL = "https://grouphoppers.azurewebsites.net/api";
 export const IP_LOCATION_URL = "https://ipinfo.io/json?token=a3ca5fd133d863";
 
 export const accessTokenLocalStorage = "accessToken";
@@ -10,5 +9,6 @@ export const userLocationLocalStorage = "userLocation";
 export const displayLocationLocalStorage = " displayLocation"
 export const userCountryLocalStorage = "userCountry"
 
-export const SignalR_URL = "https://grouphoppers.azurewebsites.net/chatHub";
-export const GOOGLE_CLIENT_ID = "48728014273-1aad0j7h1ep080sb8hr6bdtt497crb6i.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? '';
+export const SignalR_URL = process.env.REACT_APP_Signalr_URL ?? '';
+export const API_URL = process.env.REACT_APP_API_URL ?? '';
