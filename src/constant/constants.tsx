2afd5c24799a58ef31f1bd0622b89ac275551be0
Ashish Pokhrel <ashishpokherl45@gmail.com>
2025-01-22 11:22:14 -0600

    Fix constants

--- src/constant/constants.tsx
+++ src/constant/constants.tsx
@@ -6,9 +6,9 @@ export const userGuidLocalStorage = "userGuid";
 export const profileImageLocalStorage = "profileImage";
 export const tokenExpiresInLocalStorage = "tokenExpiresIn";
 export const userLocationLocalStorage = "userLocation";
-export const displayLocationLocalStorage = " displayLocation"
-export const userCountryLocalStorage = "userCountry"
-
+export const displayLocationLocalStorage = " displayLocation";
+export const userCountryLocalStorage = "userCountry";
+export const CACHE_KEY = "CACHE_KEY_BOXES";
 export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? "48728014273-1aad0j7h1ep080sb8hr6bdtt497crb6i.apps.googleusercontent.com";
 export const SignalR_URL = process.env.REACT_APP_Signalr_URL ?? 'https://grouphoppers.azurewebsites.net/chatHub';
 export const API_URL = process.env.REACT_APP_API_URL ?? 'https://grouphoppers.azurewebsites.net/api';
