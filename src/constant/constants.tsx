8222179871035abbdd4cfafe70511b40f52a1395
Ashish Pokhrel <ashishpokherl45@gmail.com>
2025-01-22 12:29:10 -0600

    Add env variables

--- src/constant/constants.tsx
+++ src/constant/constants.tsx
@@ -1,14 +1,11 @@
-export const EXPIRE_MINUTES = 1;
-export const IP_LOCATION_URL = "https://ipinfo.io/json?token=a3ca5fd133d863";
-
-export const accessTokenLocalStorage = "accessToken";
-export const userGuidLocalStorage = "userGuid";
-export const profileImageLocalStorage = "profileImage";
-export const tokenExpiresInLocalStorage = "tokenExpiresIn";
-export const userLocationLocalStorage = "userLocation";
-export const displayLocationLocalStorage = " displayLocation";
-export const userCountryLocalStorage = "userCountry";
-export const CACHE_KEY = "CACHE_KEY_BOXES";
-export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? "48728014273-1aad0j7h1ep080sb8hr6bdtt497crb6i.apps.googleusercontent.com";
-export const SignalR_URL = process.env.REACT_APP_Signalr_URL ?? 'https://grouphoppers.azurewebsites.net/chatHub';
-export const API_URL = process.env.REACT_APP_API_URL ?? 'https://grouphoppers.azurewebsites.net/api';
+export const IP_LOCATION_URL = process.env.NEXT_PUBLIC_IP_LOCATION_URL ?? '';
+export const accessTokenLocalStorage = process.env.NEXT_PUBLIC_ACCESS_TOKEN_LOCAL_STORAGE ?? '';
+export const userGuidLocalStorage = process.env.NEXT_PUBLIC_USER_GUID_LOCAL_STORAGE ?? '';
+export const profileImageLocalStorage = process.env.NEXT_PUBLIC_PROFILE_IMAGE_LOCAL_STORAGE ?? '';
+export const userLocationLocalStorage = process.env.NEXT_PUBLIC_USER_LOCATION_LOCAL_STORAGE ?? '';
+export const displayLocationLocalStorage = process.env.NEXT_PUBLIC_DISPLAY_LOCATION_LOCAL_STORAGE ?? '';
+export const userCountryLocalStorage = process.env.NEXT_PUBLIC_USER_COUNTRY_LOCAL_STORAGE ?? '';
+export const CACHE_KEY = process.env.NEXT_PUBLIC_CACHE_KEY ?? '';
+export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '';
+export const SignalR_URL = process.env.NEXT_PUBLIC_SIGNALR_URL ?? '';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? '';
